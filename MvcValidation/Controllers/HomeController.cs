fce08759e3a410696b4549df1dab2f84fe97e6a3
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-11-11 15:29:10 +0530

    mvc validation

---
+++ MvcValidation/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcValidation.Models;
+using System.Text.RegularExpressions;
+
+namespace MvcValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee employee)
+        {
+            //if (string.IsNullOrWhiteSpace(employee.Name))
+            //{
+            //    ModelState.AddModelError("Name", "Please Enter Name. ");
+            //}
+            //if (!string.IsNullOrWhiteSpace(employee.Email))
+            //{
+            //    string emailRegex = @"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$";
+            //    Regex re = new Regex(emailRegex);
+            //    if (!re.IsMatch(employee.Email))
+            //    {
+            //        ModelState.AddModelError("Email", "Please Enter Correct Email Address");
+
+            //    }
+            //}
+            //else
+            //{
+            //    ModelState.AddModelError("Email", "Please Enter Email Address");
+            //}
+            //if (string.IsNullOrWhiteSpace(employee.Mobile))
+            //{
+            //    //Mobile is textbox id 
+            //    ModelState.AddModelError("Mobile", "Please enter your mobile no");
+            //}
+            if (ModelState.IsValid)
+            {
+                return View("Index");
+
+            }
+            return View("Index");
+
+        }
+    }
+}
---
+++ MvcValidation/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace MvcValidation.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Please Enter Name.")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter Email.")]
+        [Display(Name = "Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", ErrorMessage = "Please enter correct email address.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please Enter Mbile No.")]
+        [Display(Name = "Mobile")]
+        [StringLength(10, ErrorMessage = "Mobile no must contain 10 digt.", MinimumLength = 10)]
+        public string Mobile { get; set; }
+
+    }
+}
